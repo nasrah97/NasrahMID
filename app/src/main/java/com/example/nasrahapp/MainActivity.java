a41bf199f4b38cc7fe73f1e74f73ea1cb19e2d9a
saiful8198 <saiful30412@gmail.com>
2019-12-02 14:12:50 +0700

    Initial commit

---
+++ app/src/main/java/com/example/nasrahapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.nasrahapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent nas = new Intent(MainActivity.this, Main2Activity.class);
+                MainActivity.this.startActivity(nas);
+                MainActivity.this.finish();
+
+            }
+        });
+    }
+}
